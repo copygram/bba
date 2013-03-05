5c173227e50fc5b4c877e50dafcd84ccfb58d4e9
osadi <oskar@copygr.am>
2013-03-05 09:26:32 +0100

    Cleaned up. Had to remove the template specific echo or else the file wouldn't validate the syntax.

--- app/views/BackEnd/Search/donorSearchResultsOnMap.blade.php
+++ app/views/BackEnd/Search/donorSearchResultsOnMap.blade.php
@@ -6,12 +6,12 @@
 @stop
 @section('content')
     <h2>Donor search results</h2>
-	@if($donors)
+    @if($donors)
         <div id="map_canvas"></div>
     @else
-		<h1>No donor found</h1>
-		{{{ HTML::route('donorSearch','Back to search form') }}}
-	@endif
+    <h1>No donor found</h1>
+        {{{ HTML::route('donorSearch','Back to search form') }}}
+    @endif
 @stop
 @section('scripts')
 <script type="text/javascript"
@@ -20,26 +20,25 @@
 <script type="text/javascript">
     function initialize() {
         var mapOptions = {
-            center: new google.maps.LatLng({{ $hospital->lat }}, {{ $hospital->lng }}),
+            center: new google.maps.LatLng( <?php echo $hospital->lat; ?>, <?php echo $hospital->lng; ?>),
             zoom: 12,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
-        var donors =  {{$donors}};
+        var donors =  <?php echo $donors ; ?>
         
         for (var key in donors) {
             var data = donors[key];
             var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(data.lat,data.lng),
                 map: map,
-                title: data.fname,
+                title: data.fname
             });
         }
     }
 </script>
 <script type="text/javascript">
-
     $(document).ready(function() {
         if ($('#map_canvas').length != 0) {
             initialize();
