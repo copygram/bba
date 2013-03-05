a26da4e4343b7667b53d025d1cb2761205b4cb22
osadi <oskar@copygr.am>
2013-03-05 10:01:54 +0100

    Cleaned

--- app/views/BackEnd/navbar.blade.php
+++ app/views/BackEnd/navbar.blade.php
@@ -1,30 +1,22 @@
 <div class="sidebar" id="sidebar">
         <h2>Dashboard</h2>
-        
             <ul>
                 <li><a href="{{ URL::route('home') }}">Dashboard</a></li>
                 <li><a href="#">Messages</a></li>
-                
             </ul>
-
         <h2>Donor section</h2>
-        
             <ul>
                 <li><a href="{{ URL::route('donorSearch') }}">Search Donor</a></li>
                 <li><a href="#" >New donor</a></li>
                 <li><a href="#">List of donors</a></li>
                 <li><a href="{{ URL::route('donorSearchOnMap') }}">View on map</a></li>
-                
             </ul>
-            
         <h2>User Settings</h2>
-        
             <ul>
-                <li><?php echo HTML::route('users','List of users'); ?></li>
-                <li><?php echo HTML::route('new_user','Add new user'); ?></li>
+                <li><a href="{{ URL::route('users') }}">List of users</a></li>
+                <li><a href="{{ URL::route('new_user') }}">Add new user</a></li>
                 <li><a href="#">Manage users</a></li>
                 <li><a href="#">Help </a></li>
-            </ul>   
-         
-    </div>  <!-- end of sidebar-->           
+            </ul>
+</div>  <!-- end of sidebar-->
     
