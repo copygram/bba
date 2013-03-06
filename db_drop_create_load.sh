18edc0c52bf6baf634568ae5aabb25128073f669
Linus Ekenstam <linus@copygr.am>
2013-03-06 23:19:30 +0100

    css
    puss

--- db_drop_create_load.sh
+++ db_drop_create_load.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 vagrant ssh -c 'mysql -u root -e "drop database bba;"'
 vagrant ssh -c 'mysql -u root -e "create database bba;"'
-vagrant ssh -c 'mysql -u root -D bba < /vagrant/Sites/bba/bba.sql'
+vagrant ssh -c 'mysql -u root -D bba < /vagrant/sites/bba/bba.sql'
