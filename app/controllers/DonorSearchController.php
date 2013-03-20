4fb4876244c3174270b83db715de41e39fcde664
kennedy <kennedy.otis@gmail.com>
2013-03-21 00:27:02 +0100

    Fixes #30
    -Included lastDonated field in the query search.
    -Made blood type selection in the search form to fetch dynamically.
    -Changed searchResults columns to bloodtype_id and gender_id.

--- app/controllers/DonorSearchController.php
+++ app/controllers/DonorSearchController.php
@@ -9,37 +9,40 @@ class DonorSearchController extends BaseController {
 	 */
 	public function index()
 	{
-		return View::make('BackEnd.search');
+        $bloodtypes = objectFormArray::flatten(Bloodtype::all());
+		return View::make('BackEnd.search',array('bloodtypes' => $bloodtypes));
 	}
 
     private static function getDonorsList() {
         
 
             $hospital = Hospital::find(Auth::user()->hospital_id);
-            $bloodType = Input::get('bloodtype');
+            $bloodType = Input::get('bloodtype_id');
             $poslat = $hospital->lat;
             $poslng = $hospital->lng;
             $distance = 5;
+            $allowablePeriod = 56;
 
             $donors = DB::select('SELECT *, (3959 * acos(cos
 					(radians(?)) * cos(radians(lat)) * cos(radians
 					(lng) - radians(?)) + sin(radians(?)) * sin
-					(radians(lat)))) AS distance FROM donors WHERE bloodtype = ?  HAVING distance < ? ', array($poslat, $poslng, $poslat,$bloodType,$distance));
+					(radians(lat)))) AS distance FROM donors WHERE bloodtype_id = ? AND lastDonated <= DATE_SUB(CURDATE(), INTERVAL ? DAY)  HAVING distance < ? ', array($poslat, $poslng, $poslat,$bloodType,$allowablePeriod,$distance));
 
             return $donors;
         
     }
-
+    
     private static function getAllDonors() {
         $hospital = Hospital::find(Auth::user()->hospital_id);
         $poslat = $hospital->lat;
         $poslng = $hospital->lng;
         $distance = 5;
+        $allowablePeriod = 56;
 
         $donors = DB::select('SELECT *, (3959 * acos(cos
 					(radians(?)) * cos(radians(lat)) * cos(radians
 					(lng) - radians(?)) + sin(radians(?)) * sin
-					(radians(lat)))) AS distance FROM donors HAVING distance < ? ', array($poslat, $poslng, $poslat, $distance));
+					(radians(lat)))) AS distance FROM donors WHERE lastDonated <= DATE_SUB(CURDATE(), INTERVAL ? DAY) HAVING distance < ? ', array($poslat, $poslng, $poslat, $allowablePeriod, $distance));
 
         return $donors;
     }
