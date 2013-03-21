4602d820c37d558f268cede7305c6ce55bbddd78
Oskar Adin <oskar@copygr.am>
2013-03-21 23:07:29 +0100

    Update search.blade.php

--- app/views/backend/search.blade.php
+++ app/views/backend/search.blade.php
@@ -1,25 +1,13 @@
 @extends('backend.base')
-
-@section('content')
-	
+@section('content')	
 <div class="container" style="min-height: 600px;">
     <div class="row" style="padding-top:60px;">
-
 	<form action="{{ URL::to('/admin/search') }}" method="post">
-		
-<<<<<<< Updated upstream
-	      <label for="bloodtype">Blood Type:</label><br /> 
-=======
 	      <label for="bloodtype">Blood Type:</label><br />
->>>>>>> Stashed changes
 	      {{ Form::select('bloodtype_id', $bloodtypes, Input::old('bloodtype_id')) }}
-
 	      <input  type="submit" value="Search" />
-	      {{ $errors->first('bloodtype_id','<span class="field-error">:message</span>')}}
-	    
+	      {{ $errors->first('bloodtype_id','<span class="field-error">:message</span>')}}	    
 	</form>
 </div>
-</div>	
-	
-
+</div>
 @stop
